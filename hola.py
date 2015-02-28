5f3a127bfa471251fc494005899d313998411149
rebeca <rebeca@toshiba-rebeca.(none)>
2015-02-28 18:46:18 +0100

    Clase hola

---
+++ hola.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+class hola:
+
+    def parse(self, request, rest):
+        return None
+
+    def process(self, parseRequest):
+        return("200 OK",
+               "<html><body><h1>Hola!</h1></body></html>")
+
+
+class adios:
+
+    def parse(self, request, rest):
+        return None
+
+    def process(self, parseRequest):
+        return("200 OK",
+               "<html><body><h1>Adios!</h1></body></html>")
